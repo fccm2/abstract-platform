b13b3b5b008cf06d9d249e1d4974fbb8bbbc0fb3
Florent Monnier <monnier.florent@gmail.com>
2025-05-09 16:19:15 +0200

    add Tiled sources

---
+++ absplat.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="absplat" tilewidth="64" tileheight="64" spacing="10" tilecount="384" columns="32">
+ <grid orientation="orthogonal" width="32" height="32"/>
+ <image source="Abstract_Platformer_370_assets/Vector/vector_complete.svg" width="2400" height="900"/>
+</tileset>
