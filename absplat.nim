70f4a8b3a88d02c983e1895116bcc7d76aeef8ab
Florent Monnier <monnier.florent@gmail.com>
2025-05-09 16:17:02 +0200

    adding segments for the sides, and bounds for the inertia

--- absplat.nim
+++ absplat.nim
@@ -25,8 +25,20 @@ let dt: cdouble = 0.2
 
 var c1 = Circle[void](center: Point(x: 120, y: 70), radius: 9.6, inertia: Vector(x: 0, y: 0), is_static: false)
 
+let a = Point(x: 0, y: -10)
+let b = Point(x: 0, y: 256)
+let s1 = Segment(a: a, b: b)
+
+let c = Point(x: 384, y: -10)
+let d = Point(x: 384, y: 256)
+let s2 = Segment(a: c, b: d)
+
+let e = Point(x:   0, y: -10)
+let f = Point(x: 384, y: -10)
+let s3 = Segment(a: e, b: f)
+
 var circles = @[c1]
-var segments: seq[Segment] = @[]
+var segments = @[s1, s2, s3]
 
 let tex_filename: cstring = "./Abstract_Platformer_370_assets/Vector/vector_complete.svg"
 var tex_loaded: bool = false
@@ -39,8 +51,10 @@ setImgSrc(tex, tex_filename)
 imgOnload(tex, tex_onload)
 
 proc draw_ap2(c: Circle) =
-  let cx = cint(c.center.x)
-  let cy = cint(c.center.y)
+  #let cx = cint(c.center.x)
+  #let cy = cint(c.center.y)
+  let cx = c.center.x
+  let cy = c.center.y
   let dx = cx - 9
   let dy = cy - 11
   ctx.drawImage8(tex, 1788, 21, 38, 44, dx, dy, 19, 22)
@@ -90,23 +104,29 @@ proc animate() =
         let dx = (j mod 12) * 32
         let dy = (j div 12) * 32
         ctx.drawImage8(tex, sx, sy, 64, 64, dx, dy, 32, 32)
-  for c in circles:
-    draw_ap2(c)
+    for c in circles:
+      draw_ap2(c)
 
 proc key_event(k: KeyEvent) =
   var c = addr circles[0]
   let ck = getKeyCode(k)
   case ck
   of 37: # left
-    c[].inertia.x -= 0.4
-    c[].inertia.y -= 1.2
+    c[].inertia.x -= 1.2
+    c[].inertia.y -= 1.8
+    c[].inertia.x = max(c[].inertia.x, -10.0)
+    c[].inertia.y = max(c[].inertia.y, -14.0)
   of 39: # right
-    c[].inertia.x += 0.4
-    c[].inertia.y -= 1.2
+    c[].inertia.x += 1.2
+    c[].inertia.y -= 1.8
+    c[].inertia.x = min(c[].inertia.x, 10.0)
+    c[].inertia.y = max(c[].inertia.y, -14.0)
   of 38: # up
-    c[].inertia.y -= 1.6
+    c[].inertia.y -= 2.2
+    c[].inertia.y = max(c[].inertia.y, -14.0)
   else: discard
+  #log("inertia: ", c[].inertia.x, c[].inertia.y)
 
 addKeyDownEventListener(key_event)
-setInterval(animate, 1000 div 16)
+setInterval(animate, 1000 div 18)
 
